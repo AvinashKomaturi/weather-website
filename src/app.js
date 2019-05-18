5d3477e97dfb37ccd7bd57bb80dd16a4b8d27242
Avinash.Komaturi <aviavinash0464@gmail.com>
2019-05-18 16:35:44 +0530

    Intialise

---
+++ src/app.js
@@ -0,0 +1,113 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const hbs = require("hbs");
+const geocode = require("./utils.js/geocode");
+const forecast = require("./utils.js/forecast");
+
+// Define Path for Express Config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+//setup Handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Avinash"
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Avinash"
+  });
+});
+app.get("/help", (req, res) => {
+  res.render("help", {
+    helpText: "This is some helpful Text",
+    title: "Help",
+    name: "Avinash"
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide a Address"
+    });
+  }
+  geocode(
+    req.query.address,
+    (error, { location, longitude, latitude } = {}) => {
+      if (error) {
+        return res.send({ error: error });
+      }
+      forecast(latitude, longitude, (error, data) => {
+        if (error) {
+          return res.send({ error: error });
+        }
+
+        res.send({
+          forecast: data,
+          location,
+          address: req.query.address
+        });
+      });
+    }
+  );
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide a search Term"
+    });
+  }
+  res.send({
+    products: []
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    errorMessage: "Page not found",
+    title: "404",
+    name: "Avinash"
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    errorMessage: "Page not found",
+    title: "404",
+    name: "Avinash"
+  });
+});
+app.listen(3000, () => console.log("Server is up on 3000 port"));
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(path.join(__dirname, "../public/help.html"));
+
+// app.get("", (req, res) => {
+//   res.send("<h1>Weather</h1>");
+// });
+
+// app.get("/help", (req, res) => {
+//   res.send({
+//     name: "Avinash",
+//     age: 21
+//   });
+// });
+
+// app.get("/about", (req, res) => {
+//   res.send("<h1>About</h1>");
+// });
---
+++ src/utils.js/forecast.js
@@ -0,0 +1,32 @@
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+const request = require("request");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `https://api.darksky.net/forecast/33b15f0d6187c8e8639febae667c4ebc/${latitude},${longitude}?units=si`;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to Weather Service", undefined);
+    } else if (body.error) {
+      callback("Unable to Find Location", undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.daily.data[0].summary}It is currently ${
+          body.currently.temperature
+        } degrees out.There is ${body.currently.precipProbability *
+          100}% chance of rain`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
