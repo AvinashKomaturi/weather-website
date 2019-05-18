7004a8f585f334c0d93b17036d461c942ee69496
Avinash.Komaturi <aviavinash0464@gmail.com>
2019-05-18 22:08:58 +0530

    add High and Low Temperature

--- src/utils.js/forecast.js
+++ src/utils.js/forecast.js
@@ -11,22 +11,19 @@
 const request = require("request");
 
 const forecast = (latitude, longitude, callback) => {
-  const url = `https://api.darksky.net/forecast/33b15f0d6187c8e8639febae667c4ebc/${latitude},${longitude}?units=si`;
-  request({ url, json: true }, (error, { body }) => {
-    if (error) {
-      callback("Unable to connect to Weather Service", undefined);
-    } else if (body.error) {
-      callback("Unable to Find Location", undefined);
-    } else {
-      callback(
-        undefined,
-        `${body.daily.data[0].summary}It is currently ${
-          body.currently.temperature
-        } degrees out.There is ${body.currently.precipProbability *
-          100}% chance of rain`
-      );
-    }
-  });
+    const url = `https://api.darksky.net/forecast/33b15f0d6187c8e8639febae667c4ebc/${latitude},${longitude}?units=si`;
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback("Unable to connect to Weather Service", undefined);
+        } else if (body.error) {
+            callback("Unable to Find Location", undefined);
+        } else {
+            callback(
+                undefined,
+                `${body.daily.data[0].summary}.It is currently ${body.currently.temperature} degrees out.The High today is ${body.daily.data[0].temperatureHigh} degrees and Low is ${body.daily.data[0].temperatureLow} degrees.There is ${body.currently.precipProbability * 100}% chance of rain.`
+            );
+        }
+    });
 };
 
 module.exports = forecast;
